403d6e80c957bc2c7736ed709e1c87b54005d289
Jvalant Patel <jvalant.patel@apcera.com>
2015-11-26 15:43:03 -0800

    Constant added

---
+++ src/main/java/com/actitracker/data/Constants.java
@@ -0,0 +1,13 @@
+package com.actitracker.data;
+
+/**
+ * Created by jvalantpatel on 11/26/15.
+ */
+public class Constants {
+
+    public final static Long interval = 15000L;
+    public final static Long jump = 300000L;
+    public final static double training = 0.6;
+    public final static double test = 0.4;
+
+}
